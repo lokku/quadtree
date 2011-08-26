e88b2664c4a54e3b2bd9110a1dabc724aea8a605
Mike Astle <astle@lokku.com>
2011-08-26 13:57:52 +0100

    Updateds to quadtree Makefile.

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 set -x
 
-cp libquadtree.so.1.0.1 $LOCAL_VAR/lib/
-ln -s $LOCAL_VAR/lib/libquadtree.so.1.0.1 $LOCAL_VAR/lib/libquadtree.so
-ln -s $LOCAL_VAR/lib/libquadtree.so.1.0.1 $LOCAL_VAR/lib/libquadtree.so.1
-cp *.h $LOCAL_VAR/include/
+mkdir -p $PLAYPEN_ROOT/ext/lib/
+cp libquadtree.so.1.0.1 $PLAYPEN_ROOT/ext/lib/
+ln -s $PLAYPEN_ROOT/ext/lib/libquadtree.so.1.0.1 $PLAYPEN_ROOT/ext/lib/libquadtree.so
+ln -s $PLAYPEN_ROOT/ext/lib/libquadtree.so.1.0.1 $PLAYPEN_ROOT/ext/lib/libquadtree.so.1
+cp *.h $PLAYPEN_ROOT/ext/include/
