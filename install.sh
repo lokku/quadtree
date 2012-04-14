e6fa5599dda228b41945f208f4f3f56f9c7f3c22
Vincent Launchbury <vincent@doublecreations.com>
2012-04-13 21:14:54 -0400

    GNU autotoolized the project

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -x
-
-mkdir -p $PLAYPEN_ROOT/ext/lib/
-cp libquadtree.so.1.0.1 $PLAYPEN_ROOT/ext/lib/
-ln -s $PLAYPEN_ROOT/ext/lib/libquadtree.so.1.0.1 $PLAYPEN_ROOT/ext/lib/libquadtree.so
-ln -s $PLAYPEN_ROOT/ext/lib/libquadtree.so.1.0.1 $PLAYPEN_ROOT/ext/lib/libquadtree.so.1
-cp *.h $PLAYPEN_ROOT/ext/include/
