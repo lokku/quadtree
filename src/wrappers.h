244be7225dde7aa631bf8943a8331baa6df48316
Vincent Launchbury <vincent@lokku.com>
2012-06-01 16:33:36 +0100

    Fixed huge error in new wrapper code.
    Forgot to add NO_WRAPPER guard, so the preprocessor was
    recursively substituting.

--- src/wrappers.h
+++ src/wrappers.h
@@ -10,8 +10,10 @@
 ssize_t write_wrapper(int fd, const void *buf, size_t count);
 ssize_t read_wrapper(int fd, const void *buf, size_t count);
 
-#define write(a,b,c) write_wrapper(a,b,c)
-#define read(a,b,c)  read_wrapper(a,b,c)
+#ifndef NO_WRAPPERS
+#   define write(a,b,c) write_wrapper(a,b,c)
+#   define read(a,b,c)  read_wrapper(a,b,c)
+#endif /* !NO_WRAPPERS */
 
 
 #endif /* WRAPPERS_H */
